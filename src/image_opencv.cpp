e1bff9f278859e26535781c73ec63bc8350e5226
yuqj1990 <yuqj@deepano.com>
2019-11-30 18:19:38 +0800

    ...

--- src/image_opencv.cpp
+++ src/image_opencv.cpp
@@ -9,6 +9,9 @@ using namespace cv;
 
 extern "C" {
 
+static VideoWriter video;
+int number = 0;
+
 IplImage *image_to_ipl(image im)
 {
     int x,y,c;
@@ -133,21 +136,21 @@ void make_window(char *name, int w, int h, int fullscreen)
 int save_video(image im, const char*name, int ms)
 {
     // 静态数据成员，第一次调用初始化，之后调用不会在初始化
-    static VideoWriter* video;
+    
     Mat m = image_to_mat(im);
     {
         // 空视频对象则初始化一个对象
-        if(video == NULL){
+        if(number == 0){
             const char* output_name = "output.mp4"; //修改输出路径
-            video = new VideoWriter(output_name, VideoWriter::fourcc('m', 'p', '4', 'v'), 25, Size(im.w,im.h));
+            video.open(output_name, VideoWriter::fourcc('m', 'p', '4', 'v'), 25, Size(im.w,im.h), true);
             printf("\n DST output_video = %s  \n", output_name);
+            number = 1;
         }
-        video->write(m);
+        video << m;
 		printf("\n cvWriteFrame \n");
     }
-    int c = waitKey(ms);
-    if (c != -1) c = c%256;
-    return c;
+
+    return 1;
 }
 
 }

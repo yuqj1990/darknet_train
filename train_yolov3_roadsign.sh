34152b87c4998b4a913f6cbfe61f01c1d45ab097
yuqj1990 <yuqj@deepano.com>
2020-02-23 10:46:03 +0800

    ...

--- train_yolov3_roadsign.sh
+++ train_yolov3_roadsign.sh
@@ -1,6 +1,6 @@
 
 
-./darknet detector train cfg/roadSign.data cfg/yolov3_roadsign.cfg #backup/yolov3_roadsign.backup
+./darknet detector train cfg/roadSign.data cfg/yolov3_roadsign.cfg backup/yolov3_roadsign.backup
 
 
 
---
+++ train_yolov3_roadsign_prune_9_456.sh
@@ -0,0 +1,6 @@
+
+
+./darknet detector train cfg/roadSign.data cfg/yolov3_roadsign_prune_9_456.cfg backup/
+
+
+

3b7d6f5cf7bbfce446bf61cb86603d929100ad7c
tonny Yu <Tonny.Yu@resideo.com>
2020-08-14 20:04:18 +0800

    ...

--- train_scripts/train_yolov3_person_prune_104.sh
+++ train_scripts/train_yolov3_person_prune_104.sh
@@ -1,1 +1,1 @@
-../darknet detector train ../cfg/person.data ../cfg/yolov3_person_prune_104.cfg ../backup/
+../darknet detector train -dont_show ../cfg/person.data ../cfg/yolov3_person_prune_104.cfg ../backup/

9df8038cdbbe0e3507303fd58895c278210114f9
tonny Yu <Tonny.Yu@resideo.com>
2020-08-15 15:06:10 +0800

    ...

---
+++ demo_scripts/demo_valid_person_prune_112.sh
@@ -0,0 +1,1 @@
+../darknet detector map ../cfg/person_valid.data ../cfg/yolov3_person_prune_112.cfg ../backup/yolov3_person_prune_112_final.weights
---
+++ demo_scripts/demo_video_person_prune_112.sh
@@ -0,0 +1,1 @@
+../darknet detector demo ../cfg/person.data ../cfg/yolov3_person_prune_112.cfg ../backup/yolov3_person_prune_112_final.weights

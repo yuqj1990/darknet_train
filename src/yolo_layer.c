94184f92339bdbd3314081ad9a5c1cacf76a1000
tonny Yu <Tonny.Yu@resideo.com>
2020-09-14 11:19:20 +0800

    ..

--- src/yolo_layer.c
+++ src/yolo_layer.c
@@ -365,8 +365,8 @@ void forward_yolo_layer(const layer l, network_state state)
     int class_count = 0;
     *(l.cost) = 0;
     for (b = 0; b < l.batch; ++b) {
-        float* mask_flags = (float*)xrealloc(mask_flags, l.w*l.h*sizeof(float));
-        memset(mask_flags, 0, l.w*l.h* sizeof(float));
+        float* mask_flags = (float*)xrealloc(mask_flags, 3* l.w*l.h*sizeof(float));
+        memset(mask_flags, 0, 3* l.w*l.h* sizeof(float));
         for (j = 0; j < l.h; ++j) {
             for (i = 0; i < l.w; ++i) {
                 for (n = 0; n < l.n; ++n) {
@@ -485,7 +485,7 @@ void forward_yolo_layer(const layer l, network_state state)
                 int box_index = entry_index(l, b, mask_n*l.w*l.h + j*l.w + i, 0);
                 const float class_multiplier = (l.classes_multipliers) ? l.classes_multipliers[class_id] : 1.0f;
                 ious all_ious = delta_yolo_box(truth, l.output, l.biases, best_n, box_index, i, j, l.w, l.h, state.net.w, state.net.h, l.delta, (2 - truth.w*truth.h), l.w*l.h, l.iou_normalizer * class_multiplier, l.iou_loss, 1, l.max_delta);
-                if(mask_flags[j * l.w + i] == 1)
+                if(mask_flags[mask_n*l.w*l.h + j * l.w + i] == 1)
                     printf("center merged state.net.w: %d, state.net.h: %d, stride: %d, lw: %d, lh: %d, i: %d, j: %d\n", 
                             state.net.w, state.net.h, state.net.w/l.w, l.w, l.h, i, j);
                 // range is 0 <= 1
@@ -515,7 +515,7 @@ void forward_yolo_layer(const layer l, network_state state)
                 ++class_count;
                 if (all_ious.iou > .5) recall += 1;
                 if (all_ious.iou > .75) recall75 += 1;
-                mask_flags[j * l.w + i] = 1.;
+                mask_flags[mask_n*l.w*l.h + j * l.w + i] = 1.;
             }
 
             // iou_thresh

cb239fc74cf7a848983858909b8c7410a28f38c0
tonny Yu <Tonny.Yu@resideo.com>
2020-09-17 21:35:17 +0800

    ...

---
+++ demo_scripts/demo_video_person_prune_112_v4.sh
@@ -0,0 +1,1 @@
+../darknet detector demo ../cfg/person.data ../cfg/yolov3_person_prune_112_v4.cfg ../backup/yolov3_person_prune_112_v4_final.weights
--- demo_scripts/demo_video_person_prune_224.sh
+++
@@ -1,1 +0,0 @@
-../darknet detector demo ../cfg/person.data ../cfg/yolov3_person_prune_224.cfg ../backup/yolov3_person_prune_224_final.weights
